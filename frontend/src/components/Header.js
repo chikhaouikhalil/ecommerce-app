f4a474f1cf9c61384faf28c7b9f28fa7c417d11b
chikhaouikhalil <dev.chikhaoui@gmail.com>
2021-02-20 17:25:53 +0100

    Integration of react-router with 2 screens : HomeScreen & ProductScreen

--- frontend/src/components/Header.js
+++ frontend/src/components/Header.js
@@ -1,20 +1,27 @@
 import React from "react";
 import { Navbar, Nav, Container } from "react-bootstrap";
+import { LinkContainer } from "react-router-bootstrap";
 const Header = () => {
   return (
     <header>
       <Navbar bg="dark" variant="dark" expand="md" collapseOnSelect>
         <Container>
-          <Navbar.Brand href="/">Commerce-App</Navbar.Brand>
+          <LinkContainer to="/">
+            <Navbar.Brand>Commerce-App</Navbar.Brand>
+          </LinkContainer>
           <Navbar.Toggle aria-controls="basic-navbar-nav" />
           <Navbar.Collapse id="basic-navbar-nav">
             <Nav className="ml-auto">
-              <Nav.Link href="/cart">
-                <i className="fas fa-shopping-cart pr-1"></i>Cart
-              </Nav.Link>
-              <Nav.Link href="/login">
-                <i className="fas fa-user pr-1"></i>Sign in
-              </Nav.Link>
+              <LinkContainer to="/cart">
+                <Nav.Link>
+                  <i className="fas fa-shopping-cart pr-1"></i>Cart
+                </Nav.Link>
+              </LinkContainer>
+              <LinkContainer to="/login">
+                <Nav.Link>
+                  <i className="fas fa-user pr-1"></i>Sign in
+                </Nav.Link>
+              </LinkContainer>
             </Nav>
           </Navbar.Collapse>
         </Container>
