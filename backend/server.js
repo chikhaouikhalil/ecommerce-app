6420bb112504788d7c551a687742b46c1083ee29
chikhaouikhalil <dev.chikhaoui@gmail.com>
2021-02-20 21:34:55 +0100

    set a Custom error Handling: Json error

--- backend/server.js
+++ backend/server.js
@@ -3,6 +3,7 @@ import dotenv from "dotenv";
 // the .js is a must on our files import!
 import connectDB from "./config/db.js";
 import productRoutes from "./routes/productRoutes.js";
+import { notFound, errorHandler } from "./middleware/errorMiddleware.js";
 
 dotenv.config();
 
@@ -15,6 +16,12 @@ app.get("/", (req, res) => {
 });
 
 app.use("/api/products", productRoutes);
+
+//not found error
+app.use(notFound);
+
+// custom error handler
+app.use(errorHandler);
 
 const PORT = process.env.PORT || 5000;
 app.listen(
