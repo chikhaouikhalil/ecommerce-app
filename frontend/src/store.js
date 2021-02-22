aaca5b68199afcd9a9ebc755be65c90795ed8271
chikhaouikhalil <dev.chikhaoui@gmail.com>
2021-02-22 18:15:36 +0100

    Working on CartScreen

--- frontend/src/store.js
+++ frontend/src/store.js
@@ -1,23 +1,35 @@
-import { createStore , combineReducers, applyMiddleware } from 'redux'
-import thunk from 'redux-thunk'
-import {composeWithDevTools} from 'redux-devtools-extension' 
-import {productListReducer,productDetailsReducer} from './reducers/productReducers'
+import { createStore, combineReducers, applyMiddleware } from "redux";
+import thunk from "redux-thunk";
+import { composeWithDevTools } from "redux-devtools-extension";
+import {
+  productListReducer,
+  productDetailsReducer,
+} from "./reducers/productReducers";
+import { cartReducer } from "./reducers/cartReducers";
 
 //create reducer
 const reducer = combineReducers({
-    // productList will be part of the state
-    productList:productListReducer,
-    productDetails: productDetailsReducer,
-})
+  // productList will be part of the state
+  productList: productListReducer,
+  productDetails: productDetailsReducer,
+  cart: cartReducer,
+});
+
+const cartItemsFromStorage = localStorage.getItem("cartItems")
+  ? JSON.parse(localStorage.getItem("cartItems"))
+  : [];
 
 //define initState
-const initialState= {}
+const initialState = { cart: { cartItems: cartItemsFromStorage } };
 
 // if we want to add a new middleware, just pass it in the array
-const middleware = [thunk]
+const middleware = [thunk];
 
 //create the store
-const store = createStore(reducer, initialState, composeWithDevTools(applyMiddleware(...middleware)))
+const store = createStore(
+  reducer,
+  initialState,
+  composeWithDevTools(applyMiddleware(...middleware))
+);
 
-
-export default store
+export default store;
